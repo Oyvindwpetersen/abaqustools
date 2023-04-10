178135686a054d8823a79abb452c1d9f71edac7e
Øyvind <oyvind.w.petersen@ntnu.no>
2023-04-10 11:36:04 +0200

    example

--- suspensionbridge_example/langenuen/Langenuen_test_exportmodal.py
+++ suspensionbridge_example/langenuen/Langenuen_test_exportmodal.py
@@ -3,48 +3,48 @@ import os
 import sys
 import numpy as np
 
-FolderODB='C:/Cloud\OD_OWP/Work/Python/Github/abaqustools/suspensionbridge_example/langenuen'
-JobName='Langenuen_test'
-FolderSave='C:/Cloud\OD_OWP/Work/Python/Github/abaqustools/suspensionbridge_example/langenuen'
-FolderPython='C:/Cloud/OD_OWP/Work/Python/Github/abaqustools/odbexport'
+foldername_odb='C:/Cloud\OD_OWP/Work/Python/Github/abaqustools/suspensionbridge_example/langenuen'
+jobname='Langenuen_test'
+folder_save='C:/Cloud\OD_OWP/Work/Python/Github/abaqustools/suspensionbridge_example/langenuen'
+folder_python='C:/Cloud/OD_OWP/Work/Python/Github/abaqustools/odbexport'
 prefix='Langenuen_test_export_'
 
-# Import functions for export
-sys.path.append(FolderPython)
+# Import functions for export (odbexport package)
+sys.path.append(folder_python)
 import odbfunc
 
 # Open ODB
-myOdb=odbfunc.open_odb(FolderODB,'Langenuen_test')
+odb_id=odbfunc.open_odb(foldername_odb,jobname)
 
 # Step and frames to export
-StepNumber=-1
-FrameNumber='skipfirst'
+stepnumber=-1
+framenumber='skipfirst'
 
 # Frequencies
-f=odbfunc.exporthistoryoutput(myOdb,StepNumber,'EIGFREQ')
-odbfunc.save2txt(FolderSave,'f',f,atype=1,prefix=prefix)
+f=odbfunc.exporthistoryoutput(odb_id,stepnumber,'EIGFREQ')
+odbfunc.save2txt(folder_save,'f',f,atype=1,prefix=prefix)
 
 # Generalized mass
-gm=odbfunc.exporthistoryoutput(myOdb,StepNumber,'GM')
-odbfunc.save2txt(FolderSave,'gm',gm,atype=1,prefix=prefix)
+gm=odbfunc.exporthistoryoutput(odb_id,stepnumber,'GM')
+odbfunc.save2txt(folder_save,'gm',gm,atype=1,prefix=prefix)
 
 # Mode shapes
-(phi,phi_label)=odbfunc.exportdisplacement(myOdb,StepNumber,FrameNumber=FrameNumber)
-odbfunc.save2txt(FolderSave,'phi',phi,atype=1,prefix=prefix)
-odbfunc.save2txt(FolderSave,'phi_label',phi_label,atype=2,prefix=prefix)
+(phi,phi_label)=odbfunc.exportdisplacement(odb_id,stepnumber,framenumber=framenumber)
+odbfunc.save2txt(folder_save,'phi',phi,atype=1,prefix=prefix)
+odbfunc.save2txt(folder_save,'phi_label',phi_label,atype=2,prefix=prefix)
 
 # Modal section forces
-(phi_sf,phi_sf_label)=odbfunc.exportsectionforce(myOdb,StepNumber,FrameNumber=FrameNumber)
-odbfunc.save2txt(FolderSave,'phi_sf',phi_sf,atype=1,prefix=prefix)
-odbfunc.save2txt(FolderSave,'phi_sf_label',phi_sf_label,atype=2,prefix=prefix)
+(phi_sf,phi_sf_label)=odbfunc.exportsectionforce(odb_id,stepnumber,framenumber=framenumber)
+odbfunc.save2txt(folder_save,'phi_sf',phi_sf,atype=1,prefix=prefix)
+odbfunc.save2txt(folder_save,'phi_sf_label',phi_sf_label,atype=2,prefix=prefix)
 
 # Node coordinates
-nodecoord=odbfunc.exportnodecoord(myOdb,StepNumber,0)
-odbfunc.save2txt(FolderSave,'nodecoord',nodecoord,atype=1,prefix=prefix)
+nodecoord=odbfunc.exportnodecoord(odb_id,stepnumber,0)
+odbfunc.save2txt(folder_save,'nodecoord',nodecoord,atype=1,prefix=prefix)
 
 # Element connectivity
-elconn=odbfunc.exportelconn(myOdb)
-odbfunc.save2txt(FolderSave,'elconn',elconn,atype=1,prefix=prefix)
+elconn=odbfunc.exportelconn(odb_id)
+odbfunc.save2txt(folder_save,'elconn',elconn,atype=1,prefix=prefix)
 
 # Close ODB
-odbfunc.close_odb(myOdb)
+odbfunc.close_odb(odb_id)
